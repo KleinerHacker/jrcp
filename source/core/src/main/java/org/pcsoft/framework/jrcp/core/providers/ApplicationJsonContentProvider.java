9c3ac71fb65f2c9fd5ecc4faf180819f19f7cbe1
Kleiner Hacker <Christoph.Pfeiffer.Berlin@freenet.de>
2022-03-26 20:53:08 +0100

    add a standard content provider method to builder

--- source/core/src/main/java/org/pcsoft/framework/jrcp/core/providers/ApplicationJsonContentProvider.java
+++ source/core/src/main/java/org/pcsoft/framework/jrcp/core/providers/ApplicationJsonContentProvider.java
@@ -1,5 +1,7 @@
 package org.pcsoft.framework.jrcp.core.providers;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.pcsoft.framework.jrcp.api.providers.ContentProviderType;
 import org.pcsoft.framework.jrcp.commons.exceptions.JRCPSerializationException;
 
@@ -7,7 +9,10 @@ import org.pcsoft.framework.jrcp.commons.exceptions.JRCPSerializationException;
  * Represent a content provider for content type application/json
  */
 @ContentProviderType("application/json")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ApplicationJsonContentProvider extends JsonContentProvider {
+    public static final ApplicationJsonContentProvider INSTANCE = new ApplicationJsonContentProvider();
+
     @Override
     public Object serialize(Object o) throws JRCPSerializationException {
         return null;
--- source/core/src/main/java/org/pcsoft/framework/jrcp/core/providers/TextJsonContentProvider.java
+++ source/core/src/main/java/org/pcsoft/framework/jrcp/core/providers/TextJsonContentProvider.java
@@ -1,5 +1,7 @@
 package org.pcsoft.framework.jrcp.core.providers;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.pcsoft.framework.jrcp.api.providers.ContentProviderType;
 import org.pcsoft.framework.jrcp.commons.exceptions.JRCPSerializationException;
 
@@ -7,7 +9,10 @@ import org.pcsoft.framework.jrcp.commons.exceptions.JRCPSerializationException;
  * Represent a content provider for content type text/json
  */
 @ContentProviderType("text/json")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class TextJsonContentProvider extends JsonContentProvider {
+    public static final TextJsonContentProvider INSTANCE = new TextJsonContentProvider();
+
     @Override
     public Object serialize(Object o) throws JRCPSerializationException {
         return null;
